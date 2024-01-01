6f395b8ef6243faf187738f3e0c5a36d42c00faf
Abhishek Rathore <65767695+abhisheksr1@users.noreply.github.com>
2024-01-01 17:00:35 +0000

    basics datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // Treat all js code as newer versions
+
+// alert("Hello") We are using nodejs not browsers
+
+//Datatypes
+    //Number
+    //BigInt
+    //string
+    //boolean
+    //null => standlone value type object
+    //symbol => unique
+    //object
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaScript
 Learning JavaScript
+
+# Const and Variables
+Prefer not to use var to declare variable instead use let
+and for constants use const
